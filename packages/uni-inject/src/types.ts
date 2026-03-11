91a8680a7c62ac1b3bad839d6f33977e5a37b9fd
Kriac <1651151664@qq.com>
2026-03-11 11:55:43 +0800

    feat: 新增自动注入页面配置插件

--- packages/uni-inject/src/types.ts
+++ packages/uni-inject/src/types.ts
@@ -1,7 +1,7 @@
 /** 页面路由配置 */
 export interface UniPage {
   path: string;
-  style?: unknown;
+  [key: string]: unknown;
 }
 
 /** 分包JSON配置 */
@@ -16,8 +16,32 @@ export interface UniPagesJson {
   subPackages?: UniSubPackage[];
 }
 
-/** 插件配置 */
-export interface PluginOptions {
-  /** 要注入的文件路径 - 基于项目 src 目录 */
-  injectPath?: string;
+/** 注入插件配置 */
+export interface InjectPluginOptions {
+  /**
+   * 注入的文件路径
+   * @default 'src/App.inject.vue'
+   */
+  path?: string;
+}
+
+/** 自动补全 pages 插件配置 */
+export interface AutoPagesPluginOptions {
+  /**
+   * 扫描目录
+   * @default 'pages'
+   */
+  dir?: string;
+
+  /**
+   * 分包目录
+   * @default []
+   */
+  subPackages?: string[];
+
+  /**
+   * 生成类型声明
+   * @default 'src/uni-pages.d.ts'
+   */
+  dts?: string;
 }
