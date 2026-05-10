0dfcbf154c721f70e080be3361d37fdfcdc45b8a
Kriac <1651151664@qq.com>
2026-05-10 19:58:08 +0800

    chore: 新增提交信息域

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,14 @@
 import config from "@kriac/lint-kit/commitlint";
 
 export default config({
-  scopes: [],
+  scopes: [
+    {
+      name: "示例项目",
+      value: "demo",
+    },
+    {
+      name: "注入插件",
+      value: "inject",
+    },
+  ],
 });
