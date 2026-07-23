1f3d2fa5f6213f613d353cbc04d728516b042bac
Kriac <1651151664@qq.com>
2026-07-23 17:53:31 +0800

    refactor(inject): move shared types to module-level type files

--- packages/uni-inject/src/index.ts
+++ packages/uni-inject/src/index.ts
@@ -1,5 +1,10 @@
 import uniInject from "./modules/inject";
 import uniAutoPages from "./modules/auto-pages";
 
-export type { DefinePageConfig } from "./types";
+export type {
+  AutoPagesPluginOptions,
+  DefinePageConfig,
+  UniPageStyle,
+} from "./modules/auto-pages/types";
+export type { InjectPluginOptions } from "./modules/inject/types";
 export { uniAutoPages, uniInject };
